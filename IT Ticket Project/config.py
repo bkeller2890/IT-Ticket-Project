ea4ab4dee59e123f68d59f8e12667749580d9ee1
Benjamin A Keller <107152820+bkeller2890@users.noreply.github.com>
2025-11-29 21:32:42 -0600

    Remove inner 'IT Ticket Project' folder to prepare for updated folder

--- IT Ticket Project/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# NOTE: The default SECRET_KEY is only for development. For production set
-# the SECRET_KEY environment variable to a secure random value.
-SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
-DATABASE_PATH = os.environ.get("DATABASE_PATH", "tickets.db")
